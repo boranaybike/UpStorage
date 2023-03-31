940f5ed007a9b94fe9d4b822f8df48670245f986
ik3 <aybikeboran7140@gmail.com>
2023-03-31 14:18:42 +0300

    Fluent API => One to Many & Many to Many relationships were added.

---
+++ src/Infrastructure/Persistence/Configurations/Application/CountryConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configurations.Application
+{
+    public class CountryConfiguration:IEntityTypeConfiguration<Country>
+    {
+        public void Configure(EntityTypeBuilder<Country> builder)
+        {
+            builder.HasMany<City>(x => x.Cities)
+                .WithOne(x => x.Country)
+                .HasForeignKey(x => x.CountryId);
+        }
+    }
+}
