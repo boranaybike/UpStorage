1af4e1d2976fa7ca1f2200cc972c8a1f7ee9ea67
ik3 <aybikeboran7140@gmail.com>
2023-04-08 02:46:55 +0300

    AccountCategory relationships were added.

---
+++ src/Infrastructure/Persistence/Configurations/Application/NoteConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configurations.Application
+{
+    public class NoteConfiguration : IEntityTypeConfiguration<Note>
+    {
+        public void Configure(EntityTypeBuilder<Note> builder)
+        {
+
+        }
+    }
+}
