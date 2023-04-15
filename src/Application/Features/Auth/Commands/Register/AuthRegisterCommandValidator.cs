d6f62eb66e661a3f2a1ecd8ce3f773d24904306b
ik3 <aybikeboran7140@gmail.com>
2023-04-15 21:53:45 +0300

    User's register operation was completed.

---
+++ src/Application/Features/Auth/Commands/Register/AuthRegisterCommandValidator.cs
@@ -0,0 +1,24 @@
+﻿using Application.Common.Interfaces;
+using FluentValidation;
+namespace Application.Features.Auth.Commands.Register
+{
+    public class AuthRegisterCommandValidator:AbstractValidator<AuthRegisterCommand>
+    {
+        private readonly IAuthenticationService _authenticationService;
+        public AuthRegisterCommandValidator(IAuthenticationService authenticationService)
+        {
+            _authenticationService = authenticationService;
+
+            RuleFor(x => x.Email).MustAsync(CheckIfUserExist)
+                .WithMessage("There is already an user with given email.");
+        }
+        private async Task<bool> CheckIfUserExist(string email, CancellationToken cancellationToken)
+        {
+            var doesExist = await _authenticationService.CheckIfUserExist(email, cancellationToken);
+
+            if (doesExist) return false;
+            return true;
+        }
+
+    }
+}
